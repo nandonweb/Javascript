7f206211de813f30cc5780a3873527c12f263cff
Nando <nandosilva98@outlook.com>
2022-08-01 23:16:57 -0300

    3.1

--- resumogeral.js
+++ resumogeral.js
@@ -1,9 +1,90 @@
 /* 
 RESUMO CURSO JAVASCRIPT 
 
-RESUMO 1 - BASICO
+RESUMO 1 - BASICO ----
 
-RESUMO 2 - SEGUNDO MODULO
+Print - console.log 
+Variaveis - Const, Let
+
+Dados Primitivos - String, Number, undefined, null, boolean, symbol
+
+Operadores Aritimeticos: + Soma, / DIvisão,  * Multiplicação, - Subtração, + Contatenação, 
+** Potencia / Elevado, % Resto da Divisão, Incremento / contador++, Decremento / contador--
+
+Operadores de atribuição
+contador2 +-/*= number; 
+
+Alert, Prompt, Confirm
+
+parsetInt, parsetFloor
+
+Objeto Math
+Função
+Array
+
+Valores Primitivos e valores por referencia
+
+RESUMO 2 - SEGUNDO MODULO ----
+
+Operadores de Comparação
+> maior que 
+>= maior que ou igual a
+< menor que 
+<= menor que ou igual a  
+=== igualdade estrita (valor e tipo)
+!== diferente estrito (valor e tipo)
+
+Operadores Logicos
+&& -> AND -> E / Todas as expressoes precisam ser verdadeira para retornar true
+|| -> OR -> OU / Todas as expressoes precisam ser falsa para retornar false
+! -> NOT -> NÃO 
+
+
+Curto Circuito
+&& -> false && true -> false "o valor mesmo" ex: 0
+|| -> true || false ->  vai retornar "o valor verdadeiro"
+FALSY
+*false
+0
+'' "" ``
+null / undefined 
+
+
+If Else 
+If pode ser usado sozinho
+Sempre que utilizo a palavra else, preciso de um if antes
+Eu posso ter varios else if's ba checagem
+So posso ter um else na checagem
+Podemos usar condições sem else if, utilizando apenas if e else
+// se (numero >= 0 && numero <= 5) ocorrer, faça isso (codigo)
+// se não faça isso (o codigo) 
+
+Operação Ternária
+
+Objeto Date
+
+Switch Case
+
+Atribuição via Desestruturação
+1. Arrays
+2. Objetos
+
+For - Estrutura de Repetição
+
+DOM - Arvore DOM
+
+For in - Estrutura de Repetição
+
+For of - Estrutura de Repetição
+
+While e Do - Estrutura de Repetição
+
+Break e Continue
+
+Tratando e Lançando Erros / try, catch, throw
+
+setInterval e setTimeout
+
 
 RESUMO 3 - TERCEIRO MODULO
 
