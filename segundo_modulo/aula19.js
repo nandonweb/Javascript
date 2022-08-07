15bdabc808d7928c68ed957f5b0583c59951f722
Nando <nandosilva98@outlook.com>
2022-08-07 15:50:16 -0300

    3

---
+++ segundo_modulo/aula19.js
@@ -0,0 +1,10 @@
+// Manipulaçãp de DOM 
+
+// EX: ir no console 
+// const p = document.querySelector('p');
+// p.style.color = 'red'; - Muda a cor do texto da pagina
+
+const body = document.querySelector('body');
+
+body.style.backgroundImage = "url('https://cdn.wallpapersafari.com/72/35/eJqSsc.jpg')";
+
